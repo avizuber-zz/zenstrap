933e8f1bf5e38021fae28aabdf7d2c4b12c3dd05
avizuber <avi.zuber@gmail.com>
2014-09-24 08:38:58 -0400

    updated node modules

--- app/actionpanel/actionpanel.js
+++ app/actionpanel/actionpanel.js
@@ -1,14 +1,23 @@
 'use strict';
+var $scope, $location;
+var app = angular.module('myApp.actionpanel', ['ngRoute']);
 
-angular.module('myApp.actionpanel', ['ngRoute'])
-
-.config(['$routeProvider', function($routeProvider) {
+app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/actionpanel', {
     templateUrl: 'actionpanel/actionpanel.html',
     controller: 'ActionPanelCtrl'
   });
-}])
-
-.controller('ActionPanelCtrl', [function() {
-
 }]);
+  
+app.controller('ActionPanelCtrl', ['$scope', '$location', '$anchorScroll',
+    function ($scope, $location, $anchorScroll) {
+      $scope.goToAnchor = function(id) {
+        // set the location.hash to the id of
+        // the element you wish to scroll to.
+        $location.hash(id);
+
+        // call $anchorScroll()
+        $anchorScroll();
+      };
+  }]);
+
